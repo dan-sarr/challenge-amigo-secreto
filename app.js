4fb5aae1a134a25c1c60f557ff1b20b5c89cd45a
Daniel Sarmiento Rivera <tearvid07@gmail.com>
2025-01-25 12:55:30 -0500

    Versión final
    Se añaden nuevas funcionalidades y se completa el challenge de acuerdo a las especificaciones.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ console.log(amigos)
 
 function agregarAmigo() {
     //Se obtiene el value del input
-    let amigo = document.getElementById('amigo').value;
+    let amigoInput = document.getElementById('amigo');
+    let amigo = amigoInput.value
     //Se verifica que no esté vacío
     //Si lo está, se avisa al usuario. En caso contrario, se agrega el nombre al array 'amigos'
     if (amigo == '') {
@@ -11,6 +12,7 @@ function agregarAmigo() {
     } else {
         amigos.push(amigo);
         console.log(amigos);
+        //Se limpia la caja
         amigoInput.value = '';
         //Se muestra la lista de amigos
         mostrarAmigos();
@@ -18,20 +20,33 @@ function agregarAmigo() {
 }
 
 function mostrarAmigos() {
-    // Obtener la referencia a la lista en HTML
+    // Se obtiene la referencia a la lista en HTML
     let lista = document.getElementById('listaAmigos');
 
-    // Limpiar la lista actual antes de volver a agregar los elementos
+    // Se limpia la lista actual antes de volver a agregar los elementos
     lista.innerHTML = '';
 
-    // Recorrer el array de amigos y agregar cada nombre como un <li> a la lista
+    // Itero sobre el array de amigos y se agrega cada nombre como un <li> a la lista
     for (let i = 0; i < amigos.length; i++) {
-        let li = document.createElement('li'); // Crear un nuevo <li>
-        li.textContent = amigos[i]; // Asignar el texto del <li> con el nombre del amigo
-        lista.appendChild(li); // Agregar el <li> a la lista
+        let li = document.createElement('li'); // Crea un nuevo <li>
+        li.textContent = amigos[i]; // Asigna el nombre del amigo al elemento <li> 
+        lista.appendChild(li); // Agrega el <li> a la lista
     }
 }
 
+function sortearAmigo(){
+    let randomIndex = Math.floor(Math.random() * amigos.length);
 
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
 
+    let li = document.createElement('li'); // Crear un nuevo <li>
+    li.textContent = amigos[randomIndex]; // Asignar el texto del <li> con el nombre del amigo
+    lista.appendChild(li); // Agregar el <li> a la lista
+    
+    reiniciarJuego();
+}
 
+function reiniciarJuego(){
+    amigos = [];
+}
